0de8684a7969674a233e719149260a9de7330066
Dicky Chiang <dickychiang73@gmail.com>
2016-07-26 21:19:52 +0930

    modified names of each game

--- routes/game.js
+++ routes/game.js
@@ -3,27 +3,27 @@ var router = express.Router();
 
 router.get("/game", function(req,res){
     res.render('game.ejs');
-    console.log('enter guide page...')
+    console.log('enter game page...')
 });
 
 router.get("/result", function(req,res){
     res.render('result.ejs');
-    console.log('enter guide page...')
+    console.log('enter result page...')
 });
 
-router.get("/game_alpha", function(req,res){
-	res.render('game_alpha.ejs');
-	console.log('enter game alpha...')
+router.get("/coffee_cups", function(req,res){
+	res.render('coffee_cups.ejs');
+	console.log('enter coffee_cups page...')
 });
 
-router.get("/game_beta", function(req,res){
-	res.render('game_beta.ejs');
-	console.log('enter guide beta...')
+router.get("/rubbish_rain", function(req,res){
+	res.render('rubbish_rain.ejs');
+	console.log('enter rubbish_rain page...')
 });
 
-router.get("/game_gamma", function(req,res){
-	res.render('game_gamma.ejs');
-	console.log('enter game gamma...')
+router.get("/match_color", function(req,res){
+	res.render('match_color.ejs');
+	console.log('enter match_color page...')
 });
 
 module.exports = router;
