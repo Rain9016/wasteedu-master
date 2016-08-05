fc6fbcb7140117d1d46d95c2dab2a4b8417d4146
Dicky Chiang <dickychiang73@gmail.com>
2016-08-05 13:54:26 +0930

    testing to deploy on heroku

--- server.js
+++ server.js
@@ -2,12 +2,17 @@
 var express = require("express");
 var app = express();
 var path = require("path");
+var http = require('http').Server(app);
 
 
 var bodyParser = require('body-parser');
 var game_module = require('./routes/game');
 var login_module = require('./routes/login');
 var info_module = require('./routes/info');
+
+http.listen(process.env.PORT || 3000, function(){
+  console.log('listening on', http.address().port);
+})
 
 
 app.use(bodyParser.json()); // support json encoded bodies
@@ -28,7 +33,11 @@ app.use('/', login_module);
 app.use('/', info_module);
 
 
-
+app.listen(app.get('port'), function(){
+	console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
+/*
 app.listen(3000, function(){
 	console.log('Server is running at port 3000...');
 });
+*/
