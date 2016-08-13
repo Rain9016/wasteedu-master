8e75dbb3e98c58c4381102e8f9c35599c4a6f77a
Dicky Chiang <dickychiang73@gmail.com>
2016-08-13 10:46:08 +0930

    modified all contents and paths to match the env of cordova-android

---
+++ js/gamepage.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+
+    if (document.documentElement.scrollHeight <= document.documentElement.clientHeight) {
+        bodyTag = document.getElementsByTagName('body')[0];
+        bodyTag.style.height = document.documentElement.clientWidth / screen.width * screen.height + 'px';
+    }
+    setTimeout(function() {
+        window.scrollTo(0, 1)
+    }, 0);
+};
